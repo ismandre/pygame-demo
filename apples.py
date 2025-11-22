4e571abea049209313e766c540c7e6ee9db89e06
ismandre <markoandreis07@gmail.com>
2025-11-23 00:11:42 +0100

    Add display based on the map configuration

--- apples.py
+++ apples.py
@@ -2,19 +2,23 @@ import pygame
 import sys
 from pygame.locals import *
 
+from map import MAP
+
 pygame.init()
 
 PLAYER_ASSETS = "apples_pack_60x60px"
 APPLE_ASSETS = "apples_pack_60x60px"
-OBSTACLE_ASSETS = "Dungeon assets (extract.me)/PNG/Pole/Outline/Pole_outline_green1.png"
+OBSTACLE_ASSETS = "Dungeon assets (extract.me)/PNG/Pole/Outline/Pole_outline_green2.png"
 
 font_small = pygame.font.SysFont("Verdana", 20)
 
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 700
+background = pygame.image.load("grid.jpg")
+
+SCREEN_WIDTH = 576
+SCREEN_HEIGHT = 360
 
 APPLES_IN_COL = 10
-APPLES_IN_ROW = 5
+APPLES_IN_ROW = 16
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -29,12 +33,12 @@ pygame.display.set_caption("Game")
 
 
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, center):
         super().__init__()
         self.image = pygame.image.load(OBSTACLE_ASSETS)
-        self.image = pygame.transform.scale(self.image, (60, 120))
+        self.image = pygame.transform.scale(self.image, (30, 60))
         self.rect = self.image.get_rect()
-        self.rect.center = (300, 300)
+        self.rect.center = center
 
 
 
@@ -42,20 +46,20 @@ class Apple(pygame.sprite.Sprite):
     def __init__(self, center):
         super().__init__()
         self.image = pygame.image.load(f"{APPLE_ASSETS}/apple_regular_60x60px.png")
-        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.image = pygame.transform.scale(self.image, (25, 25))
         self.rect = self.image.get_rect()
         self.rect.center = center
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, center):
         super().__init__()
         self.image = pygame.image.load(f"{PLAYER_ASSETS}/apple_golden_60x60px.png")
-        self.image = pygame.transform.scale(self.image, (40, 40))
+        self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect()
-        self.rect.center = (160, 520)
+        self.rect.center = center
 
-    def move(self, obstacle):
+    def move(self, obstacles):
         movement = 5
         pressed = pygame.key.get_pressed()
 
@@ -69,32 +73,41 @@ class Player(pygame.sprite.Sprite):
         if pressed[K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
             new_rect.y += movement
 
-        if new_rect.colliderect(obstacle.rect):
-            if pressed[K_LEFT]:
-                self.rect.left = obstacle.rect.right
-            if pressed[K_RIGHT]:
-                self.rect.right = obstacle.rect.left
-            if pressed[K_UP]:
-                self.rect.top = obstacle.rect.bottom
-            if pressed[K_DOWN]:
-                self.rect.bottom = obstacle.rect.top
-        else:
-            self.rect = new_rect
+        for obstacle in obstacles:
+            if new_rect.colliderect(obstacle.rect):
+                if pressed[K_LEFT]:
+                    self.rect.left = obstacle.rect.right
+                if pressed[K_RIGHT]:
+                    self.rect.right = obstacle.rect.left
+                if pressed[K_UP]:
+                    self.rect.top = obstacle.rect.bottom
+                if pressed[K_DOWN]:
+                    self.rect.bottom = obstacle.rect.top
+            else:
+                self.rect = new_rect
 
-P1 = Player()
-obstacle = Obstacle()
+
 
 apples = pygame.sprite.Group()
-# Create a grid of apples
+obstacles = pygame.sprite.Group()
+
 apple_grid = []
+obstacles_grid = []
 for y in range(APPLES_IN_COL):
     for x in range(APPLES_IN_ROW):
-        apple_x = int((x / APPLES_IN_ROW) * SCREEN_WIDTH) + 30
-        apple_y = int((y / APPLES_IN_COL) * SCREEN_HEIGHT) + 30
-        print(apple_x, apple_y)
-        apple = Apple((apple_x, apple_y))
-        apple_grid.append(apple)
+        apple_x = int((x / APPLES_IN_ROW) * SCREEN_WIDTH) + 15
+        apple_y = int((y / APPLES_IN_COL) * SCREEN_HEIGHT) + 15
+        if MAP[y][x] == "A":
+            apple = Apple((apple_x, apple_y))
+            apple_grid.append(apple)
+        elif MAP[y][x] == "O":
+            obstacle = Obstacle((apple_x+3, apple_y-13))
+            obstacles_grid.append(obstacle)
+        elif MAP[y][x] == "P":
+            P1 = Player((apple_x, apple_y))
+
 apples.add(apple_grid)
+obstacles.add(obstacles_grid)
 
 while True:
     for event in pygame.event.get():
@@ -102,25 +115,23 @@ while True:
             pygame.quit()
             sys.exit()
 
-    surface.fill((255, 255, 255))
+    surface.blit(background, (0, 0))
 
     scores = font_small.render(f"Score: {TOTAL_SCORE}", True, (0, 0, 0))
     surface.blit(scores, (10, 10))
 
-    surface.blit(obstacle.image, obstacle.rect)
-
     for apple in apples:
         surface.blit(apple.image, apple.rect)
+    for obstacle in obstacles:
+        surface.blit(obstacle.image, obstacle.rect)
 
     collided_apple = pygame.sprite.spritecollideany(P1, apples)
     if collided_apple:
         TOTAL_SCORE += 1
         collided_apple.kill()
 
-
-
     surface.blit(P1.image, P1.rect)
-    P1.move(obstacle)
+    P1.move(obstacles)
 
     pygame.display.update()
     clock.tick(FPS)
